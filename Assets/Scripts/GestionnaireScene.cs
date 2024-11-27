1aa235dec5d75071b61caa54f8f760fa97acc5b9
GabrielMartineau <affirmedgenie7@gmail.com>
2024-11-27 09:16:30 -0500

    Création du UI de l'accueil programmé
    L'interface est rudimentaire. Seul l'aspect technique du UI a été construit.

---
+++ Assets/Scripts/GestionnaireScene.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GestionnaireScene : MonoBehaviour
+{
+    public void ChangeScene(string nomScene)
+    {
+        SceneManager.LoadScene(nomScene);
+    }
+}
