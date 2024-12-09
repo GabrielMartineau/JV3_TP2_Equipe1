5c2091ad258ebe31fcc668771d7ee20ac30ecaf1
GabrielMartineau <affirmedgenie7@gmail.com>
2024-12-09 16:05:49 -0500

    Ajout de la vérification du nombre d'ennemis restants

--- Assets/Scripts/GestionnaireVagues.cs
+++ Assets/Scripts/GestionnaireVagues.cs
@@ -1,28 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using Unity.XR.CoreUtils;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class GestionnaireVagues : MonoBehaviour
 {  
     [SerializeField] private InfosNiveaux so_infosNiveaux;
     [SerializeField] private GestionnaireScore gestionnaireScore;
+    [SerializeField] private GestionnaireScene gestionnaireScene;
+    [SerializeField] private EnemySpawnpoint enemySpawnpoint;
+
+    public GameObject ennemis;
+
+    void Start(){
+        ennemis.name = "Ennemis";
+    }
 
     public void VerifVagueTermine()
-    {
-        
-            //Invoke("AugmenterVague­", 15f);
-            so_infosNiveaux.vague++;
-            Debug.Log($"la vague a augmenté");
-            gestionnaireScore.UpdateText();
-        
+    {        
+        //Temporaire, je dois mettre des variables pour le nombre de vagues et trouver une solution pour switcher du niveau 2 à la scène de fin sans qu'à la vague 5, il reload le niveau 2.
+            if(so_infosNiveaux.ennemisRestants == 0 && so_infosNiveaux.vague < 5){
+                Invoke("AugmenterVague­", 15.0f);
+            }
+            else if(so_infosNiveaux.ennemisRestants == 0 && so_infosNiveaux.vague == 5){
+                gestionnaireScene.ChangeScene("Niveau2");
+            }
+            else{
+                CompterEnnemisRestants();
+            }             
     }
 
     private void AugmenterVague(){
         so_infosNiveaux.vague++;
+        Debug.Log($"la vague a augmenté");
+        gestionnaireScore.UpdateText();
     }
 
     private void CompterEnnemisRestants(){
-        Debug.Log($"allo");
+        Debug.Log($"Il reste " + ennemis.transform.childCount + "ennemis restants");
+       so_infosNiveaux.ennemisRestants = ennemis.transform.childCount;
     }
 }
