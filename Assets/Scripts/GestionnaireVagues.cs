981a8a99ed7f1c87a7269a1d1d735c9ec521c2d4
GabrielMartineau <affirmedgenie7@gmail.com>
2024-12-09 16:10:30 -0500

    Avancement script vague

--- Assets/Scripts/GestionnaireVagues.cs
+++ Assets/Scripts/GestionnaireVagues.cs
@@ -10,18 +10,21 @@ public class GestionnaireVagues : MonoBehaviour
     [SerializeField] private InfosNiveaux so_infosNiveaux;
     [SerializeField] private GestionnaireScore gestionnaireScore;
     [SerializeField] private GestionnaireScene gestionnaireScene;
-    [SerializeField] private EnemySpawnpoint enemySpawnpoint;
-
     public GameObject ennemis;
 
     void Start(){
-        ennemis.name = "Ennemis";
+        ennemis.name = "Enemies";
+    }
+
+    void Update(){
+        VerifVagueTermine();
     }
 
     public void VerifVagueTermine()
     {        
         //Temporaire, je dois mettre des variables pour le nombre de vagues et trouver une solution pour switcher du niveau 2 à la scène de fin sans qu'à la vague 5, il reload le niveau 2.
             if(so_infosNiveaux.ennemisRestants == 0 && so_infosNiveaux.vague < 5){
+                Debug.Log($"vague augmente lol");
                 Invoke("AugmenterVague­", 15.0f);
             }
             else if(so_infosNiveaux.ennemisRestants == 0 && so_infosNiveaux.vague == 5){
